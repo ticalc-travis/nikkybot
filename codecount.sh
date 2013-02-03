4989dcda3fff0102cdb26a39d0be3d24b32bc8f1
Travis Evans <travisgevans@gmail.com>
2013-02-03 14:04:17 -0600

    Update script to reflect current state of things

--- codecount.sh
+++ codecount.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-pytev=$(cat ~nikkybot/bot/*.py | wc -l | cut -d ' ' -f 1)
-pyall=$(($pytev + 10125))
-ctev=4516
-call=$((4358+4516))
-shtev=$(cat ~nikkybot/bot/*.sh | wc -l | cut -d ' ' -f 1)
-shall=$shtev
-all=$(($pyall + $call + $shall))
+py=$(cat $(find -name \*.py) | wc -l | cut -d ' ' -f 1)
+c=$(cat $(find -name \*.c -or -name \*.h) | wc -l | cut -d ' ' -f 1)
+sh=$(cat $(find -name \*.sh) | wc -l | cut -d ' ' -f 1)
+all=$(($py + $c + $sh))
 
 echo "Let's see..."
-echo "$pyall lines of Python, $call lines of C, $shtev lines of shell scripts"
-echo "tev only wrote $(echo "($pytev + $ctev + $shtev)*100 / $all" | bc)% of all that, though"
+echo "$py lines of Python, $c lines of C, $sh lines of shell scripts"
+echo "$all total"
 
