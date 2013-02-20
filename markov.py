484b42089f794898009c33469d1acce4b55d6cce
Travis Evans <travisgevans@gmail.com>
2013-02-19 19:40:42 -0600

    Some whitespace changes my editor decided to randomly apply

--- markov.py
+++ markov.py
@@ -2,17 +2,17 @@
 
 # Simple Markov chain implementation
 # Copyright ©2012 Travis Evans
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,14 +22,14 @@ import shelve
 
 class Markov(object):
     """tev's Markov chain implementation"""
-    
+
     def __init__(self, order=2, case_sensitive=True,
             default_max_left_line_breaks=None, default_max_right_line_breaks=None):
         self._order = order
         self._case_sensitive = case_sensitive
         self.default_max_left_line_breaks = None
         self.default_max_right_line_breaks = None
-        
+
         self.word_forward = defaultdict(list)
         self.chain_forward = defaultdict(list)
         self.word_backward = defaultdict(list)
@@ -38,7 +38,7 @@ class Markov(object):
     def get_order(self):
         """Retrieve order of Markov chain"""
         return self._order
-        
+
     def conv_case(self, s):
         """Convert a string or sequence of strings to lowercase if case
         sensitivity is disabled"""
@@ -64,7 +64,7 @@ class Markov(object):
         if max is not None:
             return '\n'.join(string.split('\n')[0:max+1])
         return string
-        
+
     def _add(self, words, word_dict, chain_dict):
         for i, word in enumerate(words):
             order = self._order
@@ -82,7 +82,7 @@ class Markov(object):
                 l = chain_dict[chain_key]
                 l.append(chain_value)
                 chain_dict[chain_key] = l
-        
+
     def add(self, sentence):
         """Parse and add a string of words to the chain"""
         words = sentence.replace('\n', ' \n ').split(' ')
@@ -90,27 +90,27 @@ class Markov(object):
         self._add(words, self.word_forward, self.chain_forward)
         words.reverse()
         self._add(words, self.word_backward, self.chain_backward)
-        
+
     def train(self, filename):
         """Train from all lines from the given text file"""
         f = open(filename, 'r')
         for line in f:
             self.add(line)
-            
+
     def get_chain_forward(self, chain):
         """Select and return a chain from the given chain forward in context"""
         try:
             return choice(self.chain_forward[tuple(self.conv_case(chain))])
         except IndexError:
             return ()
-        
+
     def get_chain_backward(self, chain):
         """Select and return a chain from the given chain backward in context"""
         try:
             return choice(self.chain_backward[tuple(self.conv_case(chain))])
         except IndexError:
             return ()
-        
+
     def from_chain_forward(self, chain):
         """Generate a chain from the given chain forward in context"""
         out = list(chain)
@@ -121,7 +121,7 @@ class Markov(object):
             except IndexError:
                 pass
         return out
-        
+
     def from_chain_backward(self, chain):
         """Generate a chain from the given chain backward in context"""
         out = list(chain)
@@ -133,7 +133,7 @@ class Markov(object):
                 pass
         out.reverse()
         return out
-        
+
     def from_word_forward(self, word):
         """Generate a chain from the given word forward in context"""
         try:
@@ -141,7 +141,7 @@ class Markov(object):
         except IndexError:
             return ()
         return self.from_chain_forward(chain)
-        
+
     def from_word_backward(self, word):
         """Generate a chain from the given word backward in context"""
         try:
@@ -149,7 +149,7 @@ class Markov(object):
         except IndexError:
             return ()
         return self.from_chain_backward(chain)
-        
+
     def sentence_from_word(self, word, max_left_line_breaks=-1,
             max_right_line_breaks=-1):
         """Generate a full saying from the given word.  Search for a
@@ -160,12 +160,12 @@ class Markov(object):
             max_left_line_breaks = self.default_max_left_line_breaks
         if max_right_line_breaks == -1:
             max_right_line_breaks = self.default_max_right_line_breaks
-        
+
         left = ' '.join(self.from_word_backward(word)[:-1])
         left = self.adjust_left_line_breaks(left, max_left_line_breaks)
         right = ' '.join(self.from_word_forward(word)[1:])
         right = self.adjust_right_line_breaks(right, max_right_line_breaks)
-        
+
         if max_left_line_breaks is not None:
             left = '\n'.join((left.split('\n')[-max_left_line_breaks-1:]))
         if max_right_line_breaks is not None:
@@ -174,7 +174,7 @@ class Markov(object):
             return ''
             # Omit first element, which is a duplicate of the word
         return (left + ' ' + word + ' ' + right).replace(' \n ', '\n').strip()
-        
+
     def sentence_from_chain(self, forward_chain, max_left_line_breaks=-1,
             max_right_line_breaks=-1):
         """Generate a full saying from the given chain (in forward order). 
@@ -185,7 +185,7 @@ class Markov(object):
             max_left_line_breaks = self.default_max_left_line_breaks
         if max_right_line_breaks == -1:
             max_right_line_breaks = self.default_max_right_line_breaks
-        
+
         reverse_chain = tuple(reversed(forward_chain))
         left = ' '.join(self.from_chain_backward(reverse_chain))
         left = self.adjust_left_line_breaks(left, max_left_line_breaks)
