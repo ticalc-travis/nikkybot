57e23d0e0ff74271c9f881b2ca2d3964ba06cd31
Travis Evans <travisgevans@gmail.com>
2014-04-27 22:33:33 -0500

    Correct var refs for state save/loading when checking for None

--- nikkyai.py
+++ nikkyai.py
@@ -578,7 +578,7 @@ class NikkyAI(object):
 
     def load_state(self):
         """Load current internal state from DB entry"""
-        if id is None:
+        if self.id is None:
             return
         cur = self.dbconn.cursor()
         self._check_state_table()
@@ -594,7 +594,7 @@ class NikkyAI(object):
 
     def save_state(self):
         """Save current internal state to DB"""
-        if id is None:
+        if self.id is None:
             return
         cur = self.dbconn.cursor()
         self._check_state_table()
