5ad271f274d5f759b89ea5a893008488eb576068
Travis Evans <travisgevans@gmail.com>
2014-05-14 02:18:44 -0500

    I've probably gone through more IP addresses this month than I usually do in a year :P Guess I need some kind of ident system...

--- start_nikkybot.py
+++ start_nikkybot.py
@@ -104,8 +104,8 @@ if __name__ == '__main__':
                     help='Client version response to give to CTCP VERSION '
                          'requests')
     ap.add_argument('--admin-hostmasks', nargs='*', metavar='ADMIN_HOSTMASK',
-                    default=['*!ijel@ip68-102-84-166.ks.ok.cox.net',
-                             '*!travise@nvm2u.com', '*!ijel@68.102.84.166'],
+                    default=['*!ijel@ip68-102-31-111.ks.ok.cox.net',
+                             '*!travise@nvm2u.com', '*!ijel@68.102.31.111'],
                     help='Trusted hostmasks to accept special admin commands '
                          'from')
     ap.add_argument('--reconnect-wait', default=30, type=float,
