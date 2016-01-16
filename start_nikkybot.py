427390c66f06a6b0bccd2017eccbe62483e0c3d9
Travis Evans <travisgevans@gmail.com>
2016-01-16 05:25:44 -0600

    Update my IP again

--- start_nikkybot.py
+++ start_nikkybot.py
@@ -107,10 +107,10 @@ if __name__ == '__main__':
                     help='Client version response to give to CTCP VERSION '
                          'requests')
     ap.add_argument('--admin-hostmasks', nargs='*', metavar='ADMIN_HOSTMASK',
-                    default=['*!ijel@ip68-102-52-38.ks.ok.cox.net',
-                             '*!ijel@68.102.52.38', '*!travise@nvm2u.com',
-                             '*!~ijel@ip68-102-52-38.ks.ok.cox.net',
-                             '*!~ijel@68.102.52.38',],
+                    default=['*!ijel@ip68-102-79-192.ks.ok.cox.net',
+                             '*!ijel@68.102.79.192', '*!travise@nvm2u.com',
+                             '*!~ijel@ip68-102-79-192.ks.ok.cox.net',
+                             '*!~ijel@68.102.79.192',],
                     help='Trusted hostmasks to accept special admin commands '
                          'from')
     ap.add_argument('--reconnect-wait', default=30, type=float,
